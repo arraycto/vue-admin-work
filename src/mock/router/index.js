41d12949885aeacb085a5e874202a5aaaed191fe
596484713@qq.com <liufeng>
2021-02-09 17:35:00 +0800

    开始做权限功能

--- src/mock/router/index.js
+++ src/mock/router/index.js
@@ -4,42 +4,24 @@ import { baseData } from '../base'
 
 export const adminRoutes = [
   {
-    menuUrl: '/system',
-    menuName: '系统管理',
+    menuUrl: '/authority',
+    menuName: '权限管理',
     children: [
       {
-        menuUrl: '/system/',
-        menuName: '表格操作'
+        menuUrl: '/authority/department',
+        menuName: '部门管理'
       },
       {
-        menuUrl: '/normal/table-search',
-        menuName: '表格搜索'
+        menuUrl: '/authority/user',
+        menuName: '用户管理'
       },
       {
-        menuUrl: '/normal/from',
-        menuName: '表单操作'
+        menuUrl: '/authority/role',
+        menuName: '角色管理'
       },
       {
-        menuUrl: '/normal/tip',
-        menuName: '通知提示'
-      },
-      {
-        menuUrl: '/normal/grid-list',
-        menuName: '卡片列表'
-      },
-      {
-        menuUrl: '/normal/editor',
-        menuName: '编辑器',
-        children: [
-          {
-            menuUrl: '/normal/editor/richtext',
-            menuName: '富文本'
-          },
-          {
-            menuUrl: '/normal/editor/markdown',
-            menuName: 'markdown'
-          }
-        ]
+        menuUrl: '/authority/menu',
+        menuName: '菜单管理'
       }
     ]
   },
---
+++ src/views/authority/role.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>role</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
