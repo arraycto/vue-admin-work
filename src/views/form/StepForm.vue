b91e0715ee554842577c440e17e4b3723fde4a5d
mac <mac@macdeMacBook-Air-2.local>
2021-04-15 22:21:09 +0800

    分步表单

--- src/views/form/StepForm.vue
+++ src/views/form/StepForm.vue
@@ -7,6 +7,7 @@
       <el-steps
         :active="activeStep"
         align-center
+        finish-status="success"
         class="steps-wrapper"
       >
         <el-step title="填写转账信息" />
@@ -19,83 +20,50 @@
       shadow="hover"
       class="margin-top-xs"
     >
-      <div class="title text-center padding">请填写转账信息</div>
-      <el-form
-        :model="accountInfo"
-        label-width="120px"
-        class="form-wrapper"
-        size="small"
-      >
-        <el-form-item label="付款账户：">
-          <el-select
-            v-model="accountInfo.account"
-            placeholder="请输入付款账户"
-            style="width: 100%"
-          >
-            <el-option
-              label="vue-admin-work@work.com"
-              :value="accountInfo.account"
-            />
-          </el-select>
-        </el-form-item>
-        <el-form-item label="收款账户：">
-          <el-input
-            v-model="accountInfo.otherAccount"
-            placeholder="请输入收款账户"
-            class="input-with-select"
-          >
-            <el-select
-              slot="prepend"
-              v-model="accountInfo.type"
-              placeholder="请选择"
-            >
-              <el-option
-                v-for="item of accountInfo.types"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value"
-              />
-            </el-select>
-          </el-input>
-        </el-form-item>
-        <el-form-item label="收款人姓名：">
-          <el-input
-            v-model="accountInfo.receiveName"
-            placeholder="请输入收款人姓名"
-          />
-        </el-form-item>
-        <el-form-item label="转账金额：">
-          <el-input
-            v-model="accountInfo.receiveName"
-            placeholder="请输入转账金额"
-          />
-        </el-form-item>
-      </el-form>
+      <div class="title text-center padding">
+        <el-link
+          type="primary"
+          :underline="false"
+          class="text-xxl"
+        >{{ activeStep === 1 || activeStep === 2 ? '请填写转账信息' : '转账结果' }}</el-link>
+      </div>
+      <AccountInfo
+        v-if="activeStep === 1"
+        @next-step="next"
+      />
+      <PasswordInfo
+        v-if="activeStep === 2"
+        :account-info="accountInfo"
+        @next-step="activeStep++"
+        @pre-step="activeStep--"
+      />
+      <ResultInfo
+        v-if="activeStep === 3"
+        :account-info="accountInfo"
+        @pre-step="activeStep = 1"
+      />
     </el-card>
   </div>
 </template>
 
 <script>
+import AccountInfo from './components/AccountInfo'
+import PasswordInfo from './components/PasswordInfo'
+import ResultInfo from './components/ResultInfo'
 export default {
   name: 'StepForm',
+  components: { AccountInfo, PasswordInfo, ResultInfo },
   data() {
     return {
       activeStep: 1,
-      accountInfo: {
-        account: 'vue-admin-work@work.com',
-        otherAccount: '',
-        receiveName: '',
-        type: 0,
-        types: [
-          {
-            label: '微信',
-            value: 0
-          },
-          {
-            label: '支付宝',
-            value: 1
-          }
-        ]
+      accountInfo: {}
+    }
+  },
+  methods: {
+    next(accountInfo) {
+      if (this.activeStep === 1) {
+        this.accountInfo = accountInfo
+        this.activeStep += 1
       }
     }
   }
@@ -106,27 +74,15 @@ export default {
 ::v-deep .el-step__title {
   font-size: 14px;
 }
-::v-deep .input-with-select .el-input-group__prepend {
-  background-color: #fff;
-  width: 90px;
-}
 @media screen and (max-width: 768px) {
   .steps-wrapper {
     width: 100%;
-    margin: 0 auto;
-  }
-  .form-wrapper {
-    width: 90%;
     margin: 0 auto;
   }
 }
 @media screen and (min-width: 768px) {
   .steps-wrapper {
     width: 60%;
-    margin: 0 auto;
-  }
-  .form-wrapper {
-    width: 50%;
     margin: 0 auto;
   }
 }
